087983f7b4278813746e0a9f44cf3b681fea61f8
Daniel Schep <dschep@gmail.com>
2014-10-20 16:10:47 -0400

    include js in distribution

--- setup.py
+++ setup.py
@@ -47,10 +47,7 @@ setup(
 
     install_requires=['django', 'requests'],
 
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
     package_data={
-        #'sample': ['package_data.dat'],
+        'xorformfields': ['static/mutually_exclusive_widget.js'],
     },
 )
