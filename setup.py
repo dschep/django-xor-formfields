32d69b4d706405e3d315e0dae379c70b265bdb0d
Daniel Schep <dschep@gmail.com>
2014-10-27 19:01:49 -0400

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='django-xor-formfields',
 
-    version='0.0.2',
+    version='0.0.3',
 
     description='Mutually Exclusive form field wigets for Django',
     long_description=long_description,
